5ad67fb02f980e0fa11d51738e51c5a844a90ab6
dmfarcas <dan.farcas@assist.ro>
2017-05-04 16:48:17 +0300

    'temporarily' removed mock stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Harta from './Harta';
-import mock_user_locations from './mock_user_locations';
 import SidebarParticipanti from './SidebarParticipanti';
 import firebaseConfig from './config/firebase.config.js';
 import Firebase from 'firebase';
 import ReactFireMixin from 'reactfire';
 import reactMixin from 'react-mixin';
-Firebase.initializeApp(firebaseConfig);
 
-
-const participanti = mock_user_locations;
-
-
+Firebase.initializeApp(firebaseConfig
 
 class App extends Component {
   constructor(props) {
@@ -33,11 +28,8 @@ class App extends Component {
      );
   }
   componentWillMount() {
-    // Here we bind the component to Firebase and it handles all data updates,
-    // no need to poll as in the React example.
     const firebaseRef = Firebase.database().ref('Users');
-    this.bindAsArray(firebaseRef.limitToLast(25), 'participanti');
-    // console.log(this.state.participanti);
+    this.bindAsArray(firebaseRef, 'participanti');
   }
 }
 
