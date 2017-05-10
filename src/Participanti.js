b8126c224ef030a664841449a1ed7734f12a27ac
dmfarcas <dan.farcas@assist.ro>
2017-05-10 14:33:28 +0300

    Added filter by checkbox

--- src/Participanti.js
+++ src/Participanti.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
-const listaParticipanti = (participanti) => {
-  return participanti.map((p, index) => {
-    return (
-      <tr key={index}>
-        <td>{p.name}</td>
-        <td>{p.id}</td>
-        <td>{p.circuit}</td>
-      </tr>
-    );
-  });
-};
 
 export default class SidebarParticipanti extends Component {
-  static defaultProps = {
-    participanti: {}
-  };
+  constructor(props) {
+    super(props);
+    this.veziTotiParticipantii = this.props.veziTotiParticipantii.bind(this);
+    this.veziParticipantiSelectati = this.props.veziParticipantiSelectati.bind(this);
+  }
 
   render() {
+    const listaParticipanti = (participanti) => {
+      return participanti.map((p, index) => {
+        return (
+          <tr key={p.id}>
+            <td>              
+              <input
+                id={p.id}
+                name="checkboxParticipant"
+                onChange={this.veziParticipantiSelectati}
+                checked={this.props.participantiSelectati.includes(parseInt(p.id, 10))}
+                type="checkbox" />
+            </td>
+            <td>{p.name}</td>
+            <td>{p.id}</td>
+            <td>{p.circuit}</td>
+          </tr>
+        );
+      });
+    };
+
     return (
       <div>
         <table>
           <tbody>
           <tr>
+            <th>
+              <input
+                  name="veziTotiParticipantii"
+                  onChange={this.veziTotiParticipantii}
+                  checked={this.props.totiParticipantiiState}
+                  type="checkbox" />
+            </th>
             <th>Nume</th>
             <th>Numar</th>
             <th>Traseu</th>
